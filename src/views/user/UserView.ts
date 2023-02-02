f82a75917a1ae308bd9d21297cc8f9dff6ea9c63
Taiwo Babarinde <orthodox@Taiwos-MacBook-Pro.local>
2023-02-02 02:28:37 +0100

    initial commit

---
+++ src/views/user/UserView.ts
@@ -0,0 +1,18 @@
+import Vue from "vue";
+
+export default Vue.component('user-view',{
+    data() {
+        return {
+            navData: [
+                "Charter",
+                "Event",
+                "Home",
+                "My bookings",
+                "My wallet"
+            ]
+        }
+    },
+    methods: {
+
+    }
+})
---
+++ src/views/user/UserView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="w-full">
+        <nav class="bg-black">
+            <ul class="flex justify-between font-bold">
+                <li class="text-white" v-for="(data, index) in navData" :key="index">{{ data }}</li>
+            </ul>
+        </nav>
+       
+    </div>
+</template>
+
+<script lang="ts" src="./UserView.ts"></script>
