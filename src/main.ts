6ce5e0727cac5fc145255b7751777135509f6fec
Taiwo Babarinde <orthodox@Taiwos-MacBook-Pro.local>
2023-02-03 16:15:59 +0100

    changes styles

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './assets/tailwind.css'
+import './assets/css/styles.css'
 import router from './router'
 
 Vue.config.productionTip = false
